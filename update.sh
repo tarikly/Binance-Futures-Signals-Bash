0d16bc86be6f17689fc91c9beeb4d5dcb8a1a071
tarikly <tarikly+github@gmail.com>
2022-05-09 10:02:47 -0300

    fix loop and add git clone credentials

--- update.sh
+++ update.sh
@@ -10,10 +10,10 @@ do
     esac
 done
 
-[ -d "$REPO_DIR-rollback" ] && rm -rfv $REPO_DIR-rollback
+[ -d "$REPO_DIR-rollback" ] && mv $REPO_DIR-bkp-$(date '+%d-m-%Y_%H-%M')
 [ -d "REPO_DIR" ] && mv $REPO_DIR-rollback
 
-git clone https://github.com/lagoanova/Binance-Futures-Signals.git $ROOT_DIR
+#git clone https://username:password@github.com/lagoanova/Binance-Futures-Signals.git $ROOT_DIR
 
 case $1 in
      -u|--user)      
@@ -23,9 +23,16 @@ case $1 in
             cd $WORK_DIR/$USUARIO/Binance-Futures-Signals
             echo "Finalizando instancia em execucao"
             export TAG="`git rev-parse --short=10 HEAD`-$USUARIO" && docker compose --project-name $USUARIO down
+            docker inspect --format {{.Image}} $(docker ps | grep $USUARIO | awk '{print $1}') | cut -f 2 -d ":"
             [ -d "$WORK_DIR/$USUARIO/Binance-Futures-Signals-rollback " ] && rm -rf $WORK_DIR/$USUARIO/Binance-Futures-Signals-rollback 
             mv $WORK_DIR/$USUARIO/Binance-Futures-Signals $WORK_DIR/$USUARIO/Binance-Futures-Signals-rollback
-            
+          
+          else
+
+            echo "
+            Usuario nao encontrado! 
+            Crie o diretorio $WORK_DIR/$USUARIO e o arquivo $WORK_DIR/$USUARIO/.env do ambiente da app." && exit 1
+
           fi
 
           cp -av $REPO_DIR $WORK_DIR/$USUARIO/
@@ -44,11 +51,20 @@ case $1 in
             [ -d "$WORK_DIR/$i/Binance-Futures-Signals-rollback " ] && rm -rf $WORK_DIR/$i/Binance-Futures-Signals-rollback 
             mv $WORK_DIR/$i/Binance-Futures-Signals $WORK_DIR/$i/Binance-Futures-Signals-rollback
             cp -av $REPO_DIR $WORK_DIR/$i/
-            cp -av $WORK_DIR/$USUARIO/.env $WORK_DIR/$i/Binance-Futures-Signals/
+            cp -av $WORK_DIR/$i/.env $WORK_DIR/$i/Binance-Futures-Signals/
             cd $WORK_DIR/$i/Binance-Futures-Signals
             export TAG="`git rev-parse --short=10 HEAD`-$i" && docker compose --project-name $i up -d
           done
           ;;
+      -r|--rollback)
+         if [[ ! -z USUARIO ]];
+         then
+           echo variavel nao existe!
+         else
+           echo variavel existe!
+        fi
+
+        ;;
      *)
          echo "
          usage: 
